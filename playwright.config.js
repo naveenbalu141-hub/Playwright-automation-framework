3a905fec7511b812a04b2b2b183a4259f9f09588
Naveen Kumar <naveenbalu141@gmail.com>
2026-09-20 15:26:40 +0530

    'commit'

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -60,7 +60,7 @@ reporter: [
             dependencies: ['setup'] 
         },
 
-    {
+    /*{
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
@@ -91,11 +91,11 @@ reporter: [
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
+  /* Run your local dev server before starting the tests 
   // webServer: {
   //   command: 'npm run start',
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
-  // },
+  // },*/
 });
 
